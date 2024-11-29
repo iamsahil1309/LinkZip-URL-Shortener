a00dbd4051b83a668c6b07715c59c8a1369ed799
Sahil Singh <kswayam2000@gmail.com>
2024-11-29 15:21:04 +0530

    recharts for device and location info

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -9,20 +9,13 @@ import { deleteUrl } from "@/db/apiUrls";
 const LinkCard = ({url = [], fetchUrls}) => {
   const downloadImage = () => {
     const imageUrl = url?.qr;
-    const fileName = url?.title; // Desired file name for the downloaded image
-
-    // Create an anchor element
+    const fileName = url?.title;
     const anchor = document.createElement("a");
     anchor.href = imageUrl;
     anchor.download = fileName;
 
-    // Append the anchor to the body
     document.body.appendChild(anchor);
-
-    // Trigger the download by simulating a click event
     anchor.click();
-
-    // Remove the anchor from the document
     document.body.removeChild(anchor);
   };
 
--- src/pages/Link.jsx
+++ src/pages/Link.jsx
@@ -1,11 +1,154 @@
-import React from 'react'
+import DeviceStats from "@/components/DeviceStats";
+import LocationStats from "@/components/LocationStats";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { UrlState } from "@/context";
+import { getClicksForUrl } from "@/db/apiClicks";
+import { deleteUrl, getUrl } from "@/db/apiUrls";
+import useFetch from "@/hooks/useFetch";
+import { Copy, Download, LinkIcon, Trash } from "lucide-react";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BarLoader, BeatLoader } from "react-spinners";
 
 const Link = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { id } = useParams();
+  const { user } = UrlState();
+  const navigate = useNavigate();
 
-export default Link
+  const downloadImage = () => {
+    const imageUrl = url?.qr;
+    const fileName = url?.title; 
+    const anchor = document.createElement("a");
+    anchor.href = imageUrl;
+    anchor.download = fileName;
+
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  };
+
+  const {
+    loading,
+    data: url,
+    fn,
+    error,
+  } = useFetch(getUrl, { id, user_id: user?.id });
+
+  const {
+    loading: loadingStats,
+    data: stats,
+    fn: fnStats,
+  } = useFetch(getClicksForUrl, id);
+
+  const { loading: loadingDelete, fn: fnDelete } = useFetch(deleteUrl, id);
+
+  useEffect(() => {
+    fn();
+    fnStats();
+  }, []);
+
+  if (error) {
+    navigate("/dashboard");
+  }
+
+  let link = "";
+  if (url) {
+    link = url?.custom_url ? url?.custom_url : url?.short_url;
+  }
+
+  return (
+    <>
+      {(loading || loadingStats) && (
+        <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
+      )}
+      <div className="flex flex-col gap-8 sm:flex-row justify-between">
+        <div className="flex flex-col items-start gap-8 rounded-lg sm:w-2/5">
+          <span className="text-6xl font-extrabold hover:underline cursor-pointer">
+            {url?.title}
+          </span>
+          <a
+            className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
+            href={`https://linkzip.in/${link}`}
+            target="_blank"
+          >
+            https://linkzip.in/{link}
+          </a>
+          <a
+            className="flex items-center gap-1 hover:underline cursor-pointer"
+            href={url?.original_url}
+            target="_blank"
+          >
+            <LinkIcon className="p-1" />
+            {url?.original_url}
+          </a>
+          <span className="flex items-end font-extralight text-sm">
+            {new Date(url?.created_at).toLocaleString()}
+          </span>
+          <div className="flex gap-2">
+            <Button
+              variant="ghost"
+              onClick={() =>
+                navigator.clipboard.writeText(
+                  `https://linkzip.in/${url?.short_url}`
+                )
+              }
+            >
+              <Copy />
+            </Button>
+            <Button variant="ghost" onClick={downloadImage}>
+              <Download />
+            </Button>
+            <Button
+              variant="ghost"
+              onClick={() => fnDelete()}
+              disable={loadingDelete}
+            >
+              {loadingDelete ? (
+                <BeatLoader size={5} color="white" />
+              ) : (
+                <Trash />
+              )}
+            </Button>
+          </div>
+          <img
+            src={url?.qr}
+            alt="qr code"
+            className="w-full self-center sm:self-start ring ring-blue-500 p-1 object-contain"
+          />
+        </div>
+        <Card className="sm:w-3/5">
+          <CardHeader>
+            <CardTitle className="text-4xl font-extrabold">Stats</CardTitle>
+          </CardHeader>
+          {stats && stats?.length ? (
+            <CardContent className="flex flex-col gap-6">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Total Clicks</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>{stats?.length}</p>
+                </CardContent>
+              </Card>
+
+              <CardTitle>Location Data</CardTitle>
+              <LocationStats stats={stats} />
+              <CardTitle>Device info</CardTitle>
+              <DeviceStats stats={stats}/>
+
+            </CardContent>
+          ) : (
+            <CardContent>
+              {loadingStats === false
+                ? "No statistic yet"
+                : "Loading Statistics..."}
+            </CardContent>
+          )}
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default Link;
