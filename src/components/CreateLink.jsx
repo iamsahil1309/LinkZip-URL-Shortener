c8590519603ff25ab50df763015b68f30d3f11e1
Sahil Singh <kswayam2000@gmail.com>
2024-11-29 11:59:26 +0530

    create short links

---
+++ src/components/CreateLink.jsx
@@ -0,0 +1,16 @@
+import { UrlState } from '@/context'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const CreateLink = () => {
+    const {user} = UrlState()
+    const navigate = useNavigate()
+    const
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateLink
